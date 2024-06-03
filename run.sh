8203c46ddfeda7655b4692074376907cff7c3bd7
chowdary709 <chandrahari321@gmail.com>
2024-06-03 17:03:52 +0530

    automated commit #2620

---
+++ run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git pull ;
+ansible-playbook -i inventory.ini -e ansible_user=centos -e ansible_password=DevOps321 frontend.yml ;
+ansible-playbook -i inventory.ini -e ansible_user=centos -e ansible_password=DevOps321 mongodb.yml ;
+ansible-playbook -i inventory.ini -e ansible_user=centos -e ansible_password=DevOps321 catalogue.yml ;
+ansible-playbook -i inventory.ini -e ansible_user=centos -e ansible_password=DevOps321 redis.yml ;
+ansible-playbook -i inventory.ini -e ansible_user=centos -e ansible_password=DevOps321 user.yml ;
+# ansible-playbook -i inventory.ini -e ansible_user=centos -e ansible_password=DevOps321 cart.yml
